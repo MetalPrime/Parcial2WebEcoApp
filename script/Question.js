68dfff449bc6fcebb582d9070d4a26ff7b377741
Jhon Garcia <metalprime57@gmail.com>
2020-11-30 15:30:20 -0500

    update JS and delete elements

--- script/Question.js
+++ script/Question.js
@@ -7,9 +7,30 @@ class Question{
         let component = document.createElement('section');
         component.classList.add('question');
 
+        let btnDelete = document.createElement('button');
+        btnDelete.innerHTML = 'X';
+        btnDelete.classList.add('question__delete');
         component.innerHTML = `
         <div class="question__info">${this.question.description}</div>
+        <div class="question__score">
+                <p>Score: <strong class="question__average"></strong></p>
+            </div>
         `;
+
+        component.appendChild(btnDelete);
+
+        btnDelete.addEventListener('click',() =>{
+            if(this.question.isActual === true){
+                firebase.database().ref('parcial2/questions/' + this.question.id ).set({
+                    id: this.question.id,
+                    description: this.question.description,
+                    isActual: false,
+                })
+            } else {
+                firebase.database().ref('parcial2/questions/' + this.question.id ).remove();
+
+            }
+        });
         return component;
     }
 }
--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     const listHistoric = document.querySelector('.historic__list');
 
-    const inputQuestion = document.querySelector('.actual__inputQuestion')                                                                                                                                                                        
+    const inputQuestion = document.querySelector('.actual__inputQuestion')
     const btnQuestion = document.querySelector('.actual__btnOk');
 
     const actualQuestion = document.querySelector('.actual__question');
@@ -9,27 +9,51 @@ window.addEventListener('load', function(){
 
     const database = firebase.database();
 
-    btnQuestion.addEventListener('click',function(event){
+    btnQuestion.addEventListener('click', function (event) {
 
-        if(inputQuestion.value == ''){
+        let value;
+        if (inputQuestion.value == '') {
             alert('digite algo por favor');
             console.log('loco de mierda no funciona');
-        } else{
-            let questionsReference = database.ref('parcial2/questions').push();
-            let question = {
-                id: questionsReference.key,
-                description: inputQuestion.value,
-                isActual : true,
-            }
+        } else {
+
+            database.ref('parcial2/questions').once('value').then(
+                function (elem) {
+                    elem.forEach(element => {
+                        value = element.val();
+                        console.log(value);
+                    });
+
+                    database.ref('parcial2/questions/' + value.id).set({
+                        id: value.id,
+                        description: value.description,
+                        isActual: false,
+                    })
+                });
+
+                
+
+
+                let questionsReference = database.ref('parcial2/questions').push();
+                let question = {
+                    id: questionsReference.key,
+                    description: inputQuestion.value,
+                    isActual: true,
+                }
     
-            questionsReference.set(question);
-            console.log('loco de mierda')
-            inputQuestion.value = '';
+                questionsReference.set(question);
+                console.log('loco de mierda')
+                inputQuestion.value = '';
+
+            
+
+
         }
 
     });
 
-    database.ref('parcial2/questions').on('value', function(elem){
+
+    database.ref('parcial2/questions').on('value', function (elem) {
         listHistoric.innerHTML = '';
         actualQuestion.innerHTML = '';
         let newQuestion;
@@ -38,11 +62,11 @@ window.addEventListener('load', function(){
         elem.forEach(element => {
             value = element.val();
             newQuestion = new Question(value);
-            if(value.isActual){
-                actualQuestion.appendChild (newQuestion.render());
-            } else{
+            if (value.isActual) {
+                actualQuestion.appendChild(newQuestion.render());
+            } else {
                 listHistoric.appendChild(newQuestion.render());
             }
-        }); 
+        });
     });
 });
